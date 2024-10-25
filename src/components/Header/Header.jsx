f9f7063f8d6c53a5b2ff76baa37330eaa688d8e0
kovach05 <makskovach049@gmail.com>
2024-10-25 10:22:30 +0200

    added components

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import styles from './Header.css';
+
+// const Header = ({ title, subTitle }) => {
+//     return (
+//         <header className={styles.container}>
+//             <div>
+//                 <h1>{title}</h1>
+//                 <p>{subTitle}</p>
+//             </div>
+//         </header>
+//     );
+// };
+
+const Header = () => {
+    return (
+        <header>
+            <div>
+                <span className='logo'>MovieVerse</span>
+            </div>
+        </header>
+    )
+}
+
+Header.propTypes = {
+    title: PropTypes.string,
+    subTitle: PropTypes.string,
+};
+
+export default Header;
